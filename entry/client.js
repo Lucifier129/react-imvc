bfda4ef1d54ecce0b73d28598a4cde64ed2405f2
古映杰 <yj_gu@ctrip.com>
2017-12-22 14:58:56 +0800

    去掉 react render 的 log 日志

--- entry/client.js
+++ entry/client.js
@@ -8,48 +8,33 @@ const __APP_SETTINGS__ = window.__APP_SETTINGS__ || {}
 
 const getModule = module => module.default || module
 const webpackLoader = loadModule => {
-  if (typeof loadModule === 'function') {
-    return new Promise(loadModule).then(getModule)
-  }
-  return getModule(loadModule)
-}
-
-const logRenderStart = () => {
-  window.console && console.time && console.time('React#render')
-}
-
-const logRenderEnd = () => {
-  // ReactDOM.render 未必立即更新，故异步 log End
-  setTimeout(() => {
-    window.console && console.timeEnd && console.timeEnd('React#render')
-  }, 0)
+	if (typeof loadModule === 'function') {
+		return new Promise(loadModule).then(getModule)
+	}
+	return getModule(loadModule)
 }
 
 const viewEngine = {
-  render (virtualDOM, container) {
-    logRenderStart()
-    ReactDOM.render(virtualDOM, container)
-    logRenderEnd()
-  }
+	render: ReactDOM.render
 }
 
 const routes = util.getFlatList(
-  Array.isArray($routes) ? $routes : Object.values($routes)
+	Array.isArray($routes) ? $routes : Object.values($routes)
 )
 
 const appSettings = {
-  hashType: 'hashbang',
-  container: '#root',
-  ...__APP_SETTINGS__,
-  context: {
-    preload: {},
-    ...__APP_SETTINGS__.context,
-    isClient: true,
-    isServer: false
-  },
-  loader: webpackLoader,
-  routes,
-  viewEngine
+	hashType: 'hashbang',
+	container: '#root',
+	...__APP_SETTINGS__,
+	context: {
+		preload: {},
+		...__APP_SETTINGS__.context,
+		isClient: true,
+		isServer: false
+	},
+	loader: webpackLoader,
+	routes,
+	viewEngine
 }
 
 /**
@@ -57,9 +42,9 @@ const appSettings = {
  */
 const preload = {}
 Array.from(document.querySelectorAll('[data-preload]')).forEach(elem => {
-  let name = elem.getAttribute('data-preload')
-  let content = elem.textContent || elem.innerHTML
-  preload[name] = content
+	let name = elem.getAttribute('data-preload')
+	let content = elem.textContent || elem.innerHTML
+	preload[name] = content
 })
 appSettings.context.preload = preload
 
