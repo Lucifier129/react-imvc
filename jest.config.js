63932abd74a9a9e7088a3bc93b33ddbf8fc9abdc
tqma <tqma@ctrip.com>
2019-08-12 11:11:08 +0800

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,33 @@
 module.exports = {
-    // "roots": [
-    //     "./test"
-    // ],
-    "transform": {
-        "^.+\\.ts?$": "ts-jest"
+    "globals": {
+      'ts-jest': {
+        "tsConfigFile": 'tsconfig.json'
+      }
     },
-    "testMatch": ["**/test/*.ts?(x)"]
-}
+    "transform": {
+      ".(ts|tsx)": "ts-jest"
+    },
+    "testEnvironment": "jsdom",
+    "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    "moduleFileExtensions": [
+      "ts",
+      "tsx",
+      "js"
+    ],
+    "coveragePathIgnorePatterns": [
+      "/node_modules/",
+      "/test/"
+    ],
+    "coverageThreshold": {
+      "global": {
+        "branches": 90,
+        "functions": 95,
+        "lines": 95,
+        "statements": 95
+      }
+    },
+    "collectCoverageFrom": [
+      "src/*.{js,ts}"
+    ],
+    "testEnvironment": "jsdom"
+  }
