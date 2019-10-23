06d20d17f7a731eb63606ba102f57fad1bb9fcb5
tqma <tqma@ctrip.com>
2019-10-23 16:44:05 +0800

    style(hoc)

--- src/hoc/connect.tsx
+++ src/hoc/connect.tsx
@@ -9,37 +9,35 @@ export interface Props {
   [x: string]: any
 }
 
-export type ExcludeFromObject<T, E> = Pick<T, Exclude<keyof T, keyof E>>
+export interface Connect {
+  <S extends (...args: any[]) => any = () => null>(selector?: S): With<ReturnType<S>>
+}
 
-export function connect<S extends (...args: any[]) => any>(
-  selector?: S
-): <P extends object>(
-  InputComponent: React.ComponentType<P>
-) => (
-  props: ExcludeFromObject<P, ReturnType<S>>
-) => React.ReactElement {
-  return function<P extends object>(
-    InputComponent: React.ComponentType<P>
-  ): (
-    props: ExcludeFromObject<P, ReturnType<S>>
-  ) => React.ReactElement {
-    return function Connector(
-      props: ExcludeFromObject<P, ReturnType<S>>
-    ): React.ReactElement  {
-      return (
-        <GlobalContext.Consumer>
-          {({ state, handlers, actions }) => {
-              let sProps = selector ? selector({ state, handlers, actions, props }) : {}
+type ExcludeFromObject<T, E> = Pick<T, Exclude<keyof T, keyof E>>
 
-            return (
-              <InputComponent
-                {...props}
-                {...sProps}
-              />
-            )
-          }}
-        </GlobalContext.Consumer>
-      )
-    }
+export interface With<EP extends object> {
+  <P extends object>(inputComponent: React.ComponentType<P>): (props: ExcludeFromObject<P, EP>) => React.ReactElement
+}
+
+const connect: Connect = <S extends (...args: any[]) => any>(selector?: S) => (
+  InputComponent
+) => {
+  return function Connector(props) {
+    return (
+      <GlobalContext.Consumer>
+        {({ state, handlers, actions }) => {
+            let sProps = selector ? selector({ state, handlers, actions, props }) : {}
+
+          return (
+            <InputComponent
+              {...props}
+              {...sProps}
+            />
+          )
+        }}
+      </GlobalContext.Consumer>
+    )
   }
 }
+
+export default  connect
