0b787f97679d52998d6053386b733a42001c69b1
tqma <tqma@ctrip.com>
2019-10-29 16:41:15 +0800

    fix(hook): type error

--- src/hook/useCtrl.ts
+++ src/hook/useCtrl.ts
@@ -3,6 +3,9 @@ import GlobalContext from '../context'
 import Controller from '../controller/index'
 
 export default function useCtrl<Ctrl extends Controller<any, any>>() {
+  type S = Ctrl extends Controller<infer S, any> ? S : never
+  type AS = Ctrl extends Controller<any, infer AS> ? AS : never
+
   let { ctrl } = useContext(GlobalContext)
-  return ctrl as unknown as Ctrl
+  return ctrl as Controller<S, AS>
 }
