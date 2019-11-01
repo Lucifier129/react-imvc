524681ec9e6bcad9e7559727564e1816e68d9bb7
tqma <tqma@ctrip.com>
2019-11-01 14:36:47 +0800

    test(context): remove

--- test/context.test.ts
+++
@@ -1,8 +0,0 @@
-import GlobalContext from '../src/context'
-
-describe('context test', () => {
-  it('context complete', () => {
-    expect(GlobalContext.Consumer).toBeDefined()
-    expect(GlobalContext.Provider).toBeDefined()
-  })
-})
