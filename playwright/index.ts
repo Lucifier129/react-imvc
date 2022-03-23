79a5241fa99cfa89b6cc7cf080f871ccef1325a1
yj_gu <yj_gu@trip.com>
2022-03-24 00:06:46 +0800

    fix: fix playwright output dir was wrong

--- playwright/index.ts
+++ playwright/index.ts
@@ -1,48 +1,67 @@
 import * as fs from 'fs'
 import * as path from 'path'
+import * as crypto from 'crypto'
 import packageDirectory from 'pkg-dir'
 import { test as baseTest } from '@playwright/test'
 
+const uuid = () => {
+  return crypto.randomBytes(16).toString('hex')
+}
+
 const getNycOutputPath = async () => {
-  const rootDir = await packageDirectory(__dirname)
+  const rootDir = await packageDirectory()
   return path.join(rootDir ?? process.cwd(), '.nyc_output')
 }
 
-export const test = baseTest.extend({
-  context: async ({ context }, use) => {
-    try {
-      const nycOutputPath = await getNycOutputPath()
+type ContextCallbackType = Exclude<
+  Parameters<typeof baseTest.extend>[0]['context'],
+  undefined
+>
 
-      await context.addInitScript(() =>
-        window.addEventListener('beforeunload', () =>
-          (window as any).collectIstanbulCoverage(
-            JSON.stringify((window as any).__coverage__)
-          )
+const context: ContextCallbackType = async ({ context }, use) => {
+  try {
+    const nycOutputPath = await getNycOutputPath()
+
+    await context.addInitScript(() =>
+      window.addEventListener('beforeunload', () =>
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
         )
       )
+    )
 
-      await context.exposeFunction(
-        'collectIstanbulCoverage',
-        (coverageJSON: string) => {
-          if (coverageJSON) {
-            fs.mkdirSync(nycOutputPath, { recursive: true })
-            fs.writeFileSync(path.join(nycOutputPath, `out.json`), coverageJSON)
-          }
+    fs.mkdirSync(nycOutputPath, { recursive: true })
+
+    await context.exposeFunction(
+      'collectIstanbulCoverage',
+      (coverageJSON: string) => {
+        if (coverageJSON) {
+          fs.writeFileSync(
+            path.join(nycOutputPath, `${uuid()}.json`),
+            coverageJSON
+          )
         }
-      )
-
-      await use(context)
-
-      for (const page of context.pages()) {
-        await page.evaluate(() =>
-          (window as any).collectIstanbulCoverage(
-            JSON.stringify((window as any).__coverage__)
-          )
-        )
-        await page.close()
       }
-    } catch (error) {
-      console.log('collect coverage error: ', error)
+    )
+
+    await use(context)
+
+    for (const page of context.pages()) {
+      await page.evaluate(() =>
+        (window as any).collectIstanbulCoverage(
+          JSON.stringify((window as any).__coverage__)
+        )
+      )
+      await page.close()
     }
-  },
+  } catch (error) {
+    console.log('collect coverage error: ', error)
+  }
+}
+
+context.toString = () => 'async ({ context }, use) => {}'
+
+export const test = baseTest.extend({
+  // @ts-ignore
+  context: context,
 })
